345d11bba21ddd5879e30b8c2e69232a8f4c09e0
Alfonso D Rojas Hernandez <acostade@gmail.com>
2020-08-06 20:08:41 +0000

    problem resolve

--- index.js
+++ index.js
@@ -11,3 +11,24 @@
 // For example, 283 K converts to 10 °C which converts to 50 °F.
 
 // If you get stuck during this project or would like to see an experienced developer work through it, click “Get Help“ to see a project walkthrough video.
+
+
+//create a variable named kelvin and set it equal to 293
+
+const Kelvin = 293;
+
+//convert kelvin to celsius by substracting 273 from the kelvin variable
+
+const celsius = Kelvin - 273;
+
+
+//stablish the farenheit formula  
+
+let Fahrenheit = celsius * (9/5) + 32;
+
+
+
+Fahrenheit = Math.floor(Fahrenheit);
+
+
+console.log('The temperature is ' + Fahrenheit +' degrees Fahrenheit.')
